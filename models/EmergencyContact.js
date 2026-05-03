a48a1a6723de2634fe0122b314a92844b49db58d
Krithik Kumar <krithikkumar961@gmail.com>
2026-05-03 12:37:22 +0530

    Fix invalid userId crash in SOS contacts route

--- models/EmergencyContact.js
+++ models/EmergencyContact.js
@@ -1,30 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const emergencyContactSchema = new mongoose.Schema({
 
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+  userId: String,
+  name: String,
+  relation: String,
+  phone: String
 
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
+});
 
-  relation: {
-    type: String,
-    trim: true
-  },
-
-  phone: {
-    type: String,
-    required: true,
-    match: /^[0-9]{10}$/   // ensures valid 10-digit number
-  }
-
-}, { timestamps: true });
-
-module.exports = mongoose.model('EmergencyContact', emergencyContactSchema);
+module.exports = mongoose.model(
+  "EmergencyContact",
+  emergencyContactSchema
+);
